71aea0aadd31c3b20e55c775b150d9e812416db0
Sander <sander@revenberg.info>
2018-03-03 14:34:22 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+    echo $0: usage: ./install.sh  password
+    exit 0
+fi
+
+date >> /home/pi/ansible.log
+sudo apt-get update
+sudo apt-get autoremove
+
+sudo apt-get install git -y
+
+# Install Ansible and Git on the machine.
+sudo apt-get install python-pip git python-dev sshpass -y
+sudo pip install ansible
+sudo pip install markupsafe
+
+git clone https://github.com/Revenberg/ansible.git
+
+cd ansible
+./run.sh
+
+
+
+
+
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+    echo $0: usage: ./install.sh  password
+    exit 0
+fi
+
+# Configure IP address in "hosts" file. If you have more than one
+# Raspberry Pi, add more lines and enter details
+i=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+h=$(hostname)
+
+echo "$h ansible_host=$i" >> /home/pi/ansible.log
+
+echo "$i  ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass="$1 >> ~/ansible/hosts
+
+ansible-playbook main.yml >> /home/pi/ansible.log
+
