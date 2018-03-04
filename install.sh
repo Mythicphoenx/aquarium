7e549102cef0759b7eb56b069f3be4831056df99
Sander <sander@revenebrg.info>
2018-03-04 16:02:21 +0000

    Changes to be committed: 	modified:   hosts 	modified:   install.sh 	new file:   roles/camera/main.yml 	new file:   roles/camera/tasks/main.yml

--- install.sh
+++ install.sh
@@ -5,20 +5,20 @@ if [ $# -ne 1 ]; then
 fi
 
 date >> /home/pi/ansible.log
-sudo apt-get update
-sudo apt-get autoremove
+sudo apt-get update -y
+sudo apt-get autoremove -y
 
 sudo apt-get install git -y
 
 # Install Ansible and Git on the machine.
-sudo apt-get install python-pip git python-dev sshpass -y
-sudo pip install ansible
-sudo pip install markupsafe
+sudo apt-get install python3-pip python3-dev sshpass -y
+sudo pip3 install ansible
+sudo pip3 install markupsafe
 
 git clone https://github.com/Revenberg/aquarium.git
 
 cd aquarium
-./run.sh
+./run.sh $1
 
 
 
