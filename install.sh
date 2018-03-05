0bc481e6dfbdc557e0103b49287558e1caeef502
Sander <sander@revenberg.info>
2018-03-05 11:29:24 +0000

    update

--- install.sh
+++ install.sh
@@ -4,20 +4,22 @@ if [ $# -ne 1 ]; then
     exit 0
 fi
 
-date >> /home/pi/ansible.log
+date > /home/pi/ansible.log
 sudo apt-get update -y
 sudo apt-get autoremove -y
 
 sudo apt-get install git -y
 
 # Install Ansible and Git on the machine.
-sudo apt-get install python3-pip python3-dev sshpass -y
-sudo pip3 install ansible
-sudo pip3 install markupsafe
+sudo apt-get install python3-pip python3-dev sshpass -y >> /home/pi/ansible.log
+sudo pip3 install ansible >> /home/pi/ansible.log
+sudo pip3 install markupsafe >> /home/pi/ansible.log
 
 git clone https://github.com/Revenberg/aquarium.git
 
 cd aquarium
+date >> /home/pi/ansible.log
+
 ./run.sh $1
 
 
