a499019b467f8fb9965b7c96f39296b2d6bd9e77
Sander <sander@revenberg.info>
2018-03-08 05:16:19 +0000

    update

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 rm hosts
 rm *.retry
 git config --global user.name "Sander"
-git config --global user.email sander@revenberg.info
+git config --global user.email "sander@revenberg.info"
 
 git add -A *
 git commit -m "update"
