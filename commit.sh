07d37a729b361393d561cb95ffddd584cc0c4dd1
Sander <sander@revenberg.info>
2018-03-09 05:36:08 +0100

    update

--- commit.sh
+++ commit.sh
@@ -1,5 +1,8 @@
-rm hosts
-rm *.retry
+rm hosts 2>/dev/null
+rm *.retry 2>/dev/null
+rm *.log 2>/dev/null
+rm nohup.out 2>/dev/null
+
 git config --global user.name "Sander"
 git config --global user.email "sander@revenberg.info"
 
--- run.sh
+++ run.sh
@@ -14,7 +14,9 @@ echo "$i" > ~/.vault_pass.txt
 echo "[rpi]" > ~/aquarium/hosts
 echo "$i ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass="$1 >> ~/aquarium/hosts
 
-ansible-playbook main.yml ~/.vault_pass.txt
+nohub ansible-playbook main.yml ~/.vault_pass.txt > /home/pi/aquarium/ansible.log &
 
 rm /home/pi/aquarium/hosts 2>/dev/null
 rm /home/pi/aquarium/main.retry 2>/dev/null
+rm /home/pi/aquarium/nohup.out 2>/dev/null
+
