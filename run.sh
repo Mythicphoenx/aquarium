4d4bc90096b9d8c5c2cdc21cedb28ca915494fdb
Sander <sander@revenberg.info>
2018-03-09 19:37:07 +0100

    update

--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ fi
 i=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 h=$(hostname)
 
-echo "$i" > ~/.vault_pass.txt
+echo "$1" > ~/.vault_pass.txt
 
 echo "[rpi]" > ~/aquarium/hosts
-echo "$i ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass="$1 >> ~/aquarium/hosts
+echo "$i ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass="$1 --vault-password-file ~/.vault_pass.txt >> ~/aquarium/hosts
 
-nohub ansible-playbook main.yml ~/.vault_pass.txt > /home/pi/aquarium/ansible.log &
+ansible-playbook main.yml ~/.vault_pass.txt
 
 rm /home/pi/aquarium/hosts 2>/dev/null
 rm /home/pi/aquarium/main.retry 2>/dev/null
