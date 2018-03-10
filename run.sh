338aefaba98631daeb80d8ebb5421481cfae1de1
Sander <sander@revenberg.info>
2018-03-10 17:44:26 +0100

    update

--- run.sh
+++ run.sh
@@ -7,8 +7,10 @@ fi
 # Configure IP address in "hosts" file. If you have more than one
 # Raspberry Pi, add more lines and enter details
 #i=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-ping 192.168.1.1 -c 20
-i=$(arp -na | grep -i b8:27:eb | cut -d" " -f2 | sed 's/(//' | sed 's/)//')
+#ping 192.168.1.1 -c 20
+#i=$(arp -na | grep -i b8:27:eb | cut -d" " -f2 | sed 's/(//' | sed 's/)//')
+
+i=$(sudo arp-scan --interface=eth1 --localnet -r 15 | grep b8:27:eb | head -n1 | cut -d" " -f1)
 
 echo "==$i=="
 ping "$i" -c 20
