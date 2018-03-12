e993d52c4e09ee12532b68941ee57a8b4333e938
Sander <sander@revenberg.info>
2018-03-12 08:29:12 +0100

    update

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ fi
 # Raspberry Pi, add more lines and enter details
 #i=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 #ping 192.168.1.1 -c 20
-#i=$(arp -na | grep -i b8:27:eb | cut -d" " -f2 | sed 's/(//' | sed 's/)//')
+i=$(arp -na | grep -i b8:27:eb | cut -d" " -f2 | sed 's/(//' | sed 's/)//')
 
-i=$(sudo arp-scan --interface=eth1 --localnet -r 15 | grep b8:27:eb | head -n1 | cut -d" " -f1)
+#i=$(sudo arp-scan --interface=eth1 --localnet -r 15 | grep b8:27:eb | head -n1 | cut -d" " -f1)
 
 echo "==$i=="
 ping "$i" -c 20
