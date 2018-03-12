9a68c5853a1c35f319a03a0e5c6e5a6ceeca972f
Sander <sander@revenberg.info>
2018-03-12 18:48:03 +0000

    update

--- first.sh
+++ first.sh
@@ -4,6 +4,10 @@ if [ $# -ne 1 ]; then
     exit 0
 fi
 
+rpi_ip=$(grep ssh ~/hosts | cut -d' ' -f1)
+
+sshpass -p raspberry ssh $rpi_ip 'echo -e "raspberry\n'$1'\n'$1'" | passwd pi'
+
 echo "$1" > ~/.vault_pass.txt
 sed -i -e "s/PASSWRD/$1/g" ~/hosts
 
